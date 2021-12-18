a04946e57198f6c2c93a50d4d036b37a64b52739
Leo Varat <predragvarat@gmail.com>
2021-12-18 21:48:51 +0100

    Able to search for users by email

--- MyEvent/Db/DummyDatabase.cs
+++ MyEvent/Db/DummyDatabase.cs
@@ -9,35 +9,39 @@ namespace MyEvent.Db
 {
     public class DummyDatabase
     {
-        List<User> listOfUsers = new List<User>() {
+        public List<User> ListOfUsers { get; set; }
 
-            new User( )
-            {
-                Id = 1,
-                FirstName = "Urban",
-                LastName = "Karlsson",
-                Email = "urban.k@telia.se",
-                Requests = "None"
-            },
+        public DummyDatabase()
+        {
+            ListOfUsers = new List<User>() {
 
-            new User
-            {
-                Id = 2,
-                FirstName = "Obi-Wan",
-                LastName = "Kenobi",
-                Email = "not.anakin@gmail.com",
-                Requests = "Glutenallergi"
-            },
+                new User
+                {
+                    Id = 1,
+                    FirstName = "Urban",
+                    LastName = "Karlsson",
+                    Email = "urban.k@telia.se",
+                    Requests = null
+                },
 
-            new User
-            {
-                Id = 3,
-                FirstName = "Jar-Jar",
-                LastName = "Bings",
-                Email = "jarjar@hotmail.com",
-                Requests = "Alkoholfritt"
-            }
-        };
+                new User
+                {
+                    Id = 2,
+                    FirstName = "Obi-Wan",
+                    LastName = "Kenobi",
+                    Email = "not.anakin@gmail.com",
+                    Requests = "Glutenallergi"
+                },
 
+                new User
+                {
+                    Id = 3,
+                    FirstName = "Jar-Jar",
+                    LastName = "Bings",
+                    Email = "jarjar@hotmail.com",
+                    Requests = "Alkoholfritt"
+                }
+            };
+        }
     }
 }
---
+++ MyEvent/Logic/Logic.cs
@@ -0,0 +1,21 @@
+﻿using MyEvent.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEvent.Logic
+{
+    public class Logic
+    {
+        public Logic()
+        {
+            Db = new DummyDatabase();
+            SearchLogic = new SearchLogic(Db);
+        }
+
+        private DummyDatabase Db { get; set; }
+        public SearchLogic SearchLogic { get; set; }
+    }
+}
